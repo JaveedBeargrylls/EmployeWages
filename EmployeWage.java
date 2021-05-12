034908cb6d76dcf45915e565166026a29b81a5a2
JaveedBeargrylls <76989549+JaveedBeargrylls@users.noreply.github.com>
2021-05-12 23:23:49 +0530

    [Javeed] modified to switch cases

--- EmployeWage.java
+++ EmployeWage.java
@@ -1,21 +1,21 @@
 public class EmployeWage{
+public static final int IS_PART_TIME = 1;
+public static final int IS_FULL_TIME = 2;
+public static final int Wage_per_hour = 20;
 public static void main (String[] args){
 System.out.println(" Welcome To Emloyee Wage");
-int IS_FULL_TIME = 1;
-int Wage_per_hour = 20;
-int IS_PART_TIME = 2;
 int Emp_wage_hour = 0;
-double empCheck = Math.floor(Math.random()*10)%3;
-if ( empCheck == IS_FULL_TIME )
-	//System.out.println( " Employee is Present ");
-	Emp_wage_hour = 8 ;
-else if ( empCheck == IS_PART_TIME )
-	//System.out.println(" Employee is Absent ");
-	Emp_wage_hour = 4;
-else
-
-	Emp_wage_hour = 0;
-
+int empCheck = (int) Math.floor(Math.random()*10)%3;
+switch (empCheck) {
+	case IS_FULL_TIME:
+		Emp_wage_hour = 8 ;
+		break;
+	case IS_PART_TIME:
+		Emp_wage_hour = 4;
+		break;
+	default:
+		Emp_wage_hour = 0;
+}
 	int Emp = Emp_wage_hour * Wage_per_hour;
 System.out.println("Wage of a employee per day : " + Emp);
 }
